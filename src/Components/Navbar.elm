e3786cf8430c1c51e0acc09a8a030cb03edf1d33
lublanco <dmc3_lucas@hotmail.com.ar>
2018-08-16 19:52:45 -0300

    Saque espacio
    El espacio provocaba que no filtrara la lista de peliculas

--- src/Components/Navbar.elm
+++ src/Components/Navbar.elm
@@ -19,7 +19,7 @@ navbar =
     a [ class "navbar-brand", href "#" ] [ text "PdeP-Flix" ],
       Form.formInline [Styles.navbarText] [
         Checkbox.custom [ Checkbox.id "kids", Checkbox.inline, Checkbox.onCheck KidsFilter ] "Kids Protection",
-        Select.select [Select.onChange ChangeGenre] (List.map genreOption ["Genero", "terror", "suspenso","animada"]),
+        Select.select [Select.onChange ChangeGenre] (List.map genreOption ["Genero","terror","suspenso","animada"]),
         Input.text [ Input.attrs [class "mx-sm-3", placeholder "filter movies", onInput FilterName ]
       ] 
    ]
